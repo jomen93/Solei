c165820682f457326df073ebdf7cd9e762923a86
Johan <jsmendezh@unal.edu.co>
2021-08-17 14:58:42 -0500

    add first method to upload data to blob storage in azure

--- upload.py
+++ upload.py
@@ -12,18 +12,24 @@ except Exception as ex:
 	print(ex)
 
 
+# Credentials
+storage_account_name = "databizocr"
+key1 = "bNJ+owRF8uB2y6yLYScq3WpM/rU4oDmsSQuNjhDAxFcz1DtqTHxgGR0fJglq+s/4m+XFDZkWk6hwH4bpY+oBkw=="
+connect_str = "DefaultEndpointsProtocol=https;AccountName=databizocr;AccountKey=bNJ+owRF8uB2y6yLYScq3WpM/rU4oDmsSQuNjhDAxFcz1DtqTHxgGR0fJglq+s/4m+XFDZkWk6hwH4bpY+oBkw==;EndpointSuffix=core.windows.net"
 
-connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 # Create the BlobServiceClient object which will be used to create a container client
+
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
 # Create a unique name for the container
 container_name = str(uuid.uuid4())
+
 # Create the container
 container_client = blob_service_client.create_container(container_name)
 
 # Create a local directory to hold blob data
-local_path = "./data"
-os.mkdir(local_path)
+local_path = "blob/data"
+# os.mkdir(local_path)
 
 # Create a file in the local data directory to upload and download
 local_file_name = str(uuid.uuid4()) + ".txt"
@@ -31,10 +37,10 @@ upload_file_path = os.path.join(local_path, local_file_name)
 
 # Write text to the file
 file = open(upload_file_path, 'w')
-file.write("Hello, World!")
+file.write("Hello, Worldo!")
 file.close()
 
-# Create a blob client using the local file name as the name for the blob
+# # Create a blob client using the local file name as the name for the blob
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
 
 print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
