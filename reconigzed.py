6d1b66dedf3ba7b5b487195081d5c8e41e7bca51
Johan <jsmendezh@unal.edu.co>
2021-08-20 12:42:01 -0500

    modify the reconigzed function to azure services

--- reconigzed.py
+++ reconigzed.py
@@ -1,14 +1,16 @@
-
 import numpy as np 
+
+
 
 def get_cc(x):
 	numbers=[]
 	for i in range(len(x)):
-		try:
-			if len(x[i]) > 6:
-				numbers.append(int(x[i]))
-		except:
-			pass
+		for j in x[i].split():
+			try:
+				if len(j) > 6 and len(j) <=10:
+					numbers.append(int(j))
+			except:
+				pass
 			
 	numbers = np.array(numbers)
 	u, c = np.unique(numbers, return_counts=True)
@@ -16,4 +18,29 @@ def get_cc(x):
 		return u
 	else:
 		return u[c>1]
+
+
+
+# img = 48
+# for k in range(img):
+
+# 	with open("image"+str(k)+".txt") as f:
+# 	    lines = f.readlines()
+
+# 	numbers = []
+# 	for i in range(len(lines)):
+# 		for j in lines[i].split():
+# 			try:
+# 				if len(j) > 6 and len(j) <= 10:
+# 					numbers.append(int(j))
+# 			except:
+# 				pass
+
+# 	numbers = np.array(numbers)
+# 	u, c = np.unique(numbers, return_counts=True)
+# 	if len(c) == 1 and c[0] == 1:
+# 		print(u)
+# 	else:
+# 		print(u[c>1])
+
 
--- upload.py
+++ upload.py
@@ -4,6 +4,11 @@ from azure.storage.blob import BlobServiceClient
 from azure.storage.blob import BlobClient
 from azure.storage.blob import ContainerClient
 from azure.storage.blob import __version__
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
 
 try:
 	print("Azure Blob Storage v"+__version__+" - Python sample")
@@ -19,34 +24,34 @@ connect_str = "DefaultEndpointsProtocol=https;AccountName=databizocr;AccountKey=
 
 # Create the BlobServiceClient object which will be used to create a container client
 
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-# Create a unique name for the container
-container_name = str(uuid.uuid4())
+# # Create a unique name for the container
+# container_name = str(uuid.uuid4())
 
-# Create the container
-container_client = blob_service_client.create_container(container_name)
+# # Create the container
+# container_client = blob_service_client.create_container(container_name)
 
-# Create a local directory to hold blob data
-local_path = "blob/data"
-# os.mkdir(local_path)
+# # Create a local directory to hold blob data
+# local_path = "blob/data"
+# # os.mkdir(local_path)
 
-# Create a file in the local data directory to upload and download
-local_file_name = str(uuid.uuid4()) + ".txt"
-upload_file_path = os.path.join(local_path, local_file_name)
+# # Create a file in the local data directory to upload and download
+# local_file_name = str(uuid.uuid4()) + ".txt"
+# upload_file_path = os.path.join(local_path, local_file_name)
 
-# Write text to the file
-file = open(upload_file_path, 'w')
-file.write("Hello, Worldo!")
-file.close()
+# # Write text to the file
+# file = open(upload_file_path, 'w')
+# file.write("Hello, Worldo!")
+# file.close()
 
-# # Create a blob client using the local file name as the name for the blob
-blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+# # # Create a blob client using the local file name as the name for the blob
+# blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
 
-print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+# print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
 
-# Upload the created file
-with open(upload_file_path, "rb") as data:
-    blob_client.upload_blob(data)
+# # Upload the created file
+# with open(upload_file_path, "rb") as data:
+#     blob_client.upload_blob(data)
 
 
