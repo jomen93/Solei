b653032c79c425aaab1df3fc727745184d6171d4
Johan <jsmendezh@unal.edu.co>
2021-08-20 12:44:45 -0500

    organization

--- upload.py
+++ upload.py
@@ -1,57 +1,21 @@
 import os
 import uuid
-from azure.storage.blob import BlobServiceClient
-from azure.storage.blob import BlobClient
-from azure.storage.blob import ContainerClient
-from azure.storage.blob import __version__
-from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
-from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
-from msrest.authentication import CognitiveServicesCredentials
+import sys 
+
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core._match_conditions import MatchConditions
+from azure.storage.filedatalake._models import ContentSettings
+
+account_name = ""
+account_key  = ""
 
 
-try:
-	print("Azure Blob Storage v"+__version__+" - Python sample")
-except Exception as ex:
-	print("Exception:")
-	print(ex)
+def initialize_storage_account(account_name, account_key):
+	try:
+		global service_client
 
+		service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
 
-# Credentials
-storage_account_name = "databizocr"
-key1 = "bNJ+owRF8uB2y6yLYScq3WpM/rU4oDmsSQuNjhDAxFcz1DtqTHxgGR0fJglq+s/4m+XFDZkWk6hwH4bpY+oBkw=="
-connect_str = "DefaultEndpointsProtocol=https;AccountName=databizocr;AccountKey=bNJ+owRF8uB2y6yLYScq3WpM/rU4oDmsSQuNjhDAxFcz1DtqTHxgGR0fJglq+s/4m+XFDZkWk6hwH4bpY+oBkw==;EndpointSuffix=core.windows.net"
-
-# Create the BlobServiceClient object which will be used to create a container client
-
-# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-# # Create a unique name for the container
-# container_name = str(uuid.uuid4())
-
-# # Create the container
-# container_client = blob_service_client.create_container(container_name)
-
-# # Create a local directory to hold blob data
-# local_path = "blob/data"
-# # os.mkdir(local_path)
-
-# # Create a file in the local data directory to upload and download
-# local_file_name = str(uuid.uuid4()) + ".txt"
-# upload_file_path = os.path.join(local_path, local_file_name)
-
-# # Write text to the file
-# file = open(upload_file_path, 'w')
-# file.write("Hello, Worldo!")
-# file.close()
-
-# # # Create a blob client using the local file name as the name for the blob
-# blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-
-# print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
-
-# # Upload the created file
-# with open(upload_file_path, "rb") as data:
-#     blob_client.upload_blob(data)
-
-
+	except Exception as e:
+		print(e)
